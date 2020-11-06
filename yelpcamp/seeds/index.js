9fea354034b5a82a499019bc0d1f8a043cdd2a57
Sam Youatt <sdyouatt@btinternet.com>
2020-11-06 08:27:06 +0000

    yelpcamp basic styling

--- yelpcamp/seeds/index.js
+++ yelpcamp/seeds/index.js
@@ -22,14 +22,25 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint omnis nobis, ipsam, nemo doloremque facilis, debitis ut odio accusantium reiciendis consequatur tempora culpa amet voluptas quo vero ex necessitatibus quasi?',
+            price
         })
         await camp.save();
     }
 }
 
-seedDB().then(() => {
-    db.close();
-})
+seedDB()
+    .then(() => {
+        db.close();
+        console.log('seeded succesfully');
+    })
+    .catch((err) => {
+        db.close();
+        console.log('error while seeding');
+        console.log(err);
+    })
